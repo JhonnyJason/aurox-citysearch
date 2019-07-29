8e733f8ce5de8ecd43f3cc920288aca294fe4760
Lenard Frommelt <lenny09918050@googlemail.com>
2019-07-29 09:03:39 +0200

    rebuild aurox-citysearch as thingy

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "mode": "production",
+    "devtool": "none",
+    "target": "node",
+    "entry": "/home/lenny/gits/aurox-citysearch/toolset/compiled/js/index.js",
+    "output": {
+        "filename": "service.js",
+        "path": "/home/lenny/gits/aurox-citysearch/output"
+    }
+}
